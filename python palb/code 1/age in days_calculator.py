005776c777997fcef5a588ffcaaa8605ae7f8e96
abhinav7667 <abhinavmishra7667@gmail.com>
2026-01-25 19:22:58 +0530

    Add files via upload

---
+++ python palb/code 1/age in days_calculator.py
@@ -0,0 +1,3 @@
+age_years = int(input("Enter your age in years: "))
+age_days = age_years * 365
+print("Your age in days =", age_days)
---
+++ python palb/code 1/resume.py
@@ -0,0 +1,11 @@
+print('Hello! I will introduce myself.')
+
+print('Name:Abhinav')
+
+print('Age: 22')
+
+print('Job: Data Scientist')
+
+print('Address: Seoul, South Korea')
+
+print('Place of Birth: Southern California, USA')
